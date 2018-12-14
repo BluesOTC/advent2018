1a51a57671b3f68343854b1675174a1d8d240a10
Blues <dotdblues@gmail.com>
2018-12-13 22:46:59 -0500

    day 13 done, day 12 optimized

--- Day12.cs
+++ Day12.cs
@@ -10,53 +10,62 @@ namespace Advent
     {
         public static void Run(List<string> input)
         {
-            //only need to check half of the states; if arrangement is not found, must be other state
-            //array length? can maybe initialize to +21 or so, then only iterate on left-most if they become active (since things only activate for the first time if both on right are active)
+            int generations = 20000;
+            DateTime startTime = DateTime.Now;
             string state = input[0].Split(' ')[2];
             List<string> activations = new List<string>();
 
-            for(int index = 2; index < input.Count; index++)
+            for (int index = 2; index < input.Count; index++)
             {
                 if (input[index][9] == '#')
                     activations.Add(input[index].Substring(0, 5));
             }
 
-            StringBuilder currGeneration = new StringBuilder();
-            currGeneration.Append(new string('.', 21));
+            StringBuilder currGeneration = new StringBuilder("..");
             currGeneration.Append(state);
-            currGeneration.Append(new string('.', 210));
-            Console.WriteLine(currGeneration.ToString().Trim('.'));
+            currGeneration.Append(new string('.', 2));
+            int offset = 2;
 
             HashSet<string> patternsSeen = new HashSet<string>();
-            for(int generation = 0; generation < 500; generation++) //edit generation number after first run to see how plant pattern shifts
+            for (int generation = 0; generation < generations; generation++) //edit generation number after first run to see how plant pattern shifts
             {
                 char[] currState = new char[currGeneration.Length];
                 currGeneration.CopyTo(0, currState, 0, currGeneration.Length);
                 StringBuilder nextGeneration = new StringBuilder(new string(currState));
-                for (int index = 2; index < currGeneration.Length - 3; index++)
+                for (int index = 2; index < nextGeneration.Length - 2; index++)
                 {
                     if (activations.Contains(currGeneration.ToString().Substring(index - 2, 5)))
                         nextGeneration[index] = '#';
                     else
                         nextGeneration[index] = '.';
                 }
+                if (nextGeneration.ToString().IndexOf('#') <= 2)
+                {
+                    nextGeneration.Insert(0, "..");
+                    offset += 2;
+                }
+                if (nextGeneration.ToString().LastIndexOf('#') >= nextGeneration.Length - 3)
+                    nextGeneration.Append("..");
                 currGeneration = nextGeneration;
-                if (patternsSeen.Contains(currGeneration.ToString().Trim('.')))
+                /*if (patternsSeen.Contains(currGeneration.ToString().Trim('.')))
                 {
                     Console.WriteLine(String.Format("Repeat Pattern found at generation {0}", generation));
-                    Console.WriteLine(currGeneration.ToString().Trim('.'));
                     Console.WriteLine(String.Format("First plant found at pot #{0}", currGeneration.ToString().IndexOf('#')));
-                    break;
+                    //break;
                 }
                 else
-                    patternsSeen.Add(currGeneration.ToString().Trim('.'));
-                Console.WriteLine(currGeneration.ToString().Trim('.'));
+                    patternsSeen.Add(currGeneration.ToString().Trim('.'));*/
             }
 
             int total = 0;
+            int plants = 0;
             for (int index = 0; index < currGeneration.Length; index++)
-                total += (currGeneration[index] == '#' ? 1 : 0) * (index - 21);
-            Console.WriteLine("Sum Product: " + total);
+            {
+                plants++;
+                total += (currGeneration[index] == '#' ? 1 : 0) * (index - offset);
+            } 
+            Console.WriteLine("Sum Product: " + total + " from " + plants + " plants");
+            Console.WriteLine(DateTime.Now - startTime);
         }
     }
 }
---
+++ Day13.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent
+{
+    class Cart : IComparable<Cart>
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        DirectionType heading { get; set; }
+        int nextIntersection { get; set; }
+        public char currTrack;
+
+        public Cart(int x, int y, DirectionType heading)
+        {
+            this.x = x;
+            this.y = y;
+            this.heading = heading;
+            nextIntersection = 1;
+            switch(heading)
+            {
+                case DirectionType.NORTH:
+                case DirectionType.SOUTH:
+                    currTrack = '|';
+                    break;
+                case DirectionType.EAST:
+                case DirectionType.WEST:
+                    currTrack = '-';
+                    break;
+            }
+        }
+
+        public bool move(ref List<char[]> grid) //change cart locations to proper symbol
+        {
+            grid[y][x] = currTrack;
+            advance();
+            currTrack = grid[y][x];
+            if (currTrack == 'c')
+                return true;
+            else if (currTrack == '+')
+                doIntersection();
+            else
+            {
+                switch (heading)
+                {
+                    case DirectionType.NORTH:
+                        if (currTrack == '/')
+                            heading = DirectionType.EAST;
+                        else if (currTrack == '\\')
+                            heading = DirectionType.WEST;
+                        break;
+                    case DirectionType.EAST:
+                        if (currTrack == '/')
+                            heading = DirectionType.NORTH;
+                        else if (currTrack == '\\')
+                            heading = DirectionType.SOUTH;
+                        break;
+                    case DirectionType.SOUTH:
+                        if (currTrack == '/')
+                            heading = DirectionType.WEST;
+                        else if (currTrack == '\\')
+                            heading = DirectionType.EAST;
+                        break;
+                    case DirectionType.WEST:
+                        if (currTrack == '/')
+                            heading = DirectionType.SOUTH;
+                        else if (currTrack == '\\')
+                            heading = DirectionType.NORTH;
+                        break;
+                }
+            }
+            grid[y][x] = 'c';
+            return false;
+        }
+
+        void advance()
+        {
+            switch (heading)
+            {
+                case DirectionType.NORTH:
+                    y = y - 1;
+                    break;
+                case DirectionType.EAST:
+                    x = x + 1;
+                    break;
+                case DirectionType.SOUTH:
+                    y = y + 1;
+                    break;
+                case DirectionType.WEST:
+                    x = x - 1;
+                    break;
+            }
+        }
+
+        void doIntersection()
+        {
+            switch(nextIntersection)
+            {
+                case 1:
+                    if (heading == DirectionType.NORTH)
+                        heading = DirectionType.WEST;
+                    else
+                        heading = heading - 1;
+                    break;
+                case 3:
+                    if (heading == DirectionType.WEST)
+                        heading = DirectionType.NORTH;
+                    else
+                        heading = heading + 1;
+                    nextIntersection = 0;
+                    break;
+            }
+            nextIntersection++;
+        }
+
+        int IComparable<Cart>.CompareTo(Cart other)
+        {
+            if (this.y != other.y)
+                return this.y - other.y;
+            return this.x - other.x;
+        }
+    }
+
+    public enum DirectionType
+    {
+        NORTH,
+        EAST,
+        SOUTH,
+        WEST,
+        NUM_TYPES
+    }
+
+    class Day13
+    {
+        public static void Run(List<string> input)
+        {
+            List<char[]> grid = new List<char[]>();
+            List<char[]> origGrid = new List<char[]>();
+            List<Cart> carts = new List<Cart>();
+
+            for (int y = 0; y < input.Count; y++)
+            {
+                grid.Add(input[y].ToCharArray());
+                origGrid.Add(input[y].ToCharArray());
+                for (int x = 0; x < input[0].Length; x++)
+                {
+                    if (grid[y][x] == '<')
+                    {
+                        carts.Add(new Cart(x, y, DirectionType.WEST));
+                        grid[y][x] = 'c';
+                        origGrid[y][x] = '-';
+                    }
+                    else if (grid[y][x] == '>')
+                    {
+                        carts.Add(new Cart(x, y, DirectionType.EAST));
+                        grid[y][x] = 'c';
+                        origGrid[y][x] = '-';
+                    }
+                    else if (grid[y][x] == 'v')
+                    {
+                        carts.Add(new Cart(x, y, DirectionType.SOUTH));
+                        grid[y][x] = 'c';
+                        origGrid[y][x] = '|';
+                    }
+                    else if (grid[y][x] == '^')
+                    {
+                        carts.Add(new Cart(x, y, DirectionType.NORTH));
+                        grid[y][x] = 'c';
+                        origGrid[y][x] = '|';
+                    }
+                }
+            }
+
+            bool hasCrashed = false;
+            while (carts.Count > 1)
+            {
+                List<Cart> next = new List<Cart>(carts);
+                carts.Sort();
+                foreach (Cart c in carts)
+                {
+                    if (!next.Contains(c))
+                        continue;
+                    if (hasCrashed = c.move(ref grid))
+                    {
+                        int removed = next.RemoveAll(x => x.x == c.x && x.y == c.y);
+                        grid[c.y][c.x] = origGrid[c.y][c.x];
+                        //Console.WriteLine("Crashed! " + c.x + "," + c.y);
+                        //break;
+                    }
+                }
+                carts = next;
+            }
+            Console.WriteLine("One cart left! " + carts[0].x + "," + carts[0].y);
+        }
+    }
+}
