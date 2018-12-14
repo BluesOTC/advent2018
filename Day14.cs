fcd94dfd064114d223ffb0fca6bc1437df2593e6
Blues <dotdblues@gmail.com>
2018-12-14 10:14:00 -0500

    code cleanup

--- Day14.cs
+++ Day14.cs
@@ -15,69 +15,30 @@ namespace Advent
             int elf2 = 1;
             int recipes = 2;
             int index = 0;
-            int[] input = new int[] { 6, 3, 3, 6, 0, 1 };
-            //int[] input = new int[] { 0, 1, 2, 4, 5 };
+            //int[] input = new int[] { 6, 3, 3, 6, 0, 1 };
+            int[] input = new int[] { 0, 1, 2, 4, 5 };
 
-            while (true /*recipes <= 633611*/)
+            //while (recipes <= 633611)
+            while(index < input.Length)
             {
                 int sum = scoreboard[elf1] + scoreboard[elf2];
                 if (sum > 9)
                 {
+                    recipes++;
                     scoreboard.Add(1);
-                    recipes += 2;
+                    index = input[index] == 1 ? index + 1 : 0;
+                    if (index >= input.Length)
+                        break;
+                    recipes++;
+                    index = input[index] == sum % 10 ? index + 1 : input[0] == sum % 10 ? 1 : 0;
                     scoreboard.Add(sum % 10);
                 }
                 else
                 {
-                    scoreboard.Add(sum);
                     recipes++;
+                    scoreboard.Add(sum);
+                    index = input[index] == sum ? index + 1 : 0;
                 }
-                if (sum == input[index])
-                {
-                    index++;
-                    if (index >= input.Length)
-                        break;
-                }
-                else if(sum > 9)
-                {
-                    if (input[index] == 1)
-                    {
-                        index++;
-                        if (index >= input.Length)
-                        {
-                            index++;
-                            break;
-                        }
-                        if (input[index] == sum % 10)
-                        {
-                            index++;
-                            if (index >= input.Length)
-                                break;
-                        }
-                        else
-                        {
-                            index = 0;
-                            if (input[index] == sum % 10)
-                            {
-                                index++;
-                                if (index >= input.Length)
-                                    break;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        index = 0;
-                        if (input[index] == sum % 10)
-                        {
-                            index++;
-                            if (index >= input.Length)
-                                break;
-                        }
-                    }
-                }
-                else
-                    index = 0;
                 elf1 = (elf1 + scoreboard[elf1] + 1) % scoreboard.Count;
                 elf2 = (elf2 + scoreboard[elf2] + 1) % scoreboard.Count;
             }
