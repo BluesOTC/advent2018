40613487842866e4c2bece1cff709dd4cfe65059
Blues <dotdblues@gmail.com>
2018-12-17 12:38:28 -0500

    conversion to summed tables, needs debugging

--- Day11.cs
+++ Day11.cs
@@ -28,7 +28,6 @@ namespace Advent
             Console.WriteLine("Day 11");
 
             int serial = 7689;
-            int gridTotal = 0;
             int[][] grid = new int[300][];
             for (int i = 0; i < 300; i++)
                 grid[i] = Enumerable.Repeat(0, 300).ToArray();
@@ -37,57 +36,61 @@ namespace Advent
                 for (int j = 1; j < 301; j++)
                 {
                     grid[i - 1][j - 1] = (new PowerCoordinate(i, j)).getPower(serial);
-                    gridTotal += grid[i - 1][j - 1];
+                    if (i > 1)
+                    {
+                        grid[i - 1][j - 1] += grid[i - 2][j - 1];
+                        if (j > 1)
+                        {
+                            grid[i - 1][j - 1] += grid[i - 1][j - 2];
+                            grid[i - 1][j - 1] -= grid[i - 2][j - 2];
+                        }
+                    }
+                    else if (j > 1)
+                        grid[i - 1][j - 1] += grid[i - 1][j - 2];
                 }
             }
 
-            int[][] candidates = new int[298][];
-            for (int i = 0; i < 298; i++)
-                candidates[i] = new int[298];
             int maxPower = Int32.MinValue;
             Coordinate maxCorner = null;
 
-            for (int i = 0; i < candidates.Length; i++)
+            int maxLength = 3;
+            int length = 3;
+            for (; length < grid.Length; length++)
             {
-                for (int j = 1; j < candidates.Length; j++)
+                int maxPowerLength = 0;
+                Coordinate maxPowerCorner = null; 
+                for (int i = length; i < grid.Length; i++)
                 {
-                    candidates[i][j] = grid[i][j] + grid[i][j + 1] + grid[i][j + 2] + grid[i + 1][j] + grid[i + 1][j + 1] + grid[i + 1][j + 2] + grid[i + 2][j] + grid[i + 2][j + 1] + grid[i + 2][j + 2];
-                    if (candidates[i][j] > maxPower)
+                    for (int j = length; j < grid[i].Length; j++)
                     {
-                        maxPower = candidates[i][j];
-                        maxCorner = new Coordinate(i + 1, j + 1);
+                        int areaPower = grid[i][j];
+                        if (i > length - 1)
+                        {
+                            areaPower -= grid[i - length][j];
+                            if (j > length)
+                            {
+                                areaPower -= grid[i][j - length];
+                                areaPower += grid[i - length][j - length];
+                            }
+                        }
+                        else if (j > length - 1)
+                            areaPower -= grid[i][j - length];
+                        if (areaPower > maxPower)
+                        {
+                            maxPowerLength = areaPower;
+                            maxPowerCorner = new Coordinate(i + 1, j + 1);
+                        }
                     }
                 }
-            }
-            Console.WriteLine(String.Format("Part 1: {0}: Power = {1}", maxCorner.ToString(), maxPower));
-
-            int maxLength = 3;
-            for (int length = 4; length < 286; length++) //max power at 3x3 was 31, so 3x3 is the minimum possible optimal size. total of all grid power values was -40310, so the largest possible area is (90000 - 40341/5) square units, or 286x286 or smaller
-            {
-                int maxPowerLength = 31;
-                Coordinate maxCornerLength = null;
-                for (int i = 0; i < candidates.Length - length; i++)
+                if(length == 3)
                 {
-                    for (int j = 0; j < candidates.Length - length; j++)
-                    {
-                        for (int addIndex = 0; addIndex < length; addIndex++)
-                        {
-                            candidates[i][j] += grid[i + addIndex][j + length - 1];
-                            candidates[i][j] += grid[i + length - 1][j + addIndex];
-                        }
-
-                        if (candidates[i][j] > maxPowerLength)
-                        {
-                            maxPowerLength = candidates[i][j];
-                            maxCornerLength = new Coordinate(i + 1, j + 1);
-                        }
-                    }
+                    Console.WriteLine(String.Format("Part 1: {0}: Power = {1}", maxPowerCorner.ToString(), maxPowerLength));
                 }
 
                 if (maxPowerLength > maxPower)
                 {
                     maxPower = maxPowerLength;
-                    maxCorner = maxCornerLength;
+                    maxCorner = maxPowerCorner;
                     maxLength = length;
                 }
             }
