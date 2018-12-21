7e216f4e5723f6e803214579ed9c103d0f7760dd
Blues <dotdblues@gmail.com>
2018-12-21 02:34:07 -0500

    delete old day 20 code

--- Day20.cs
+++ Day20.cs
@@ -16,15 +16,6 @@ namespace Advent
             List<string> backtracks = new List<string> { "NS", "SN", "EW", "WE" };
             using (StreamReader reader = new StreamReader("input/input20.txt"))
                 regexMap = new StringBuilder(reader.ReadLine());
-            /*int length;
-            do
-            {
-                length = regexMap.Length;
-                foreach (string match in backtracks)
-                    regexMap.Replace(match, "");
-            } while (regexMap.Length < length);
-            int index = 1;
-            Console.WriteLine(traverse(ref index));*/
 
             HashSet<Coordinate> coordinates = new HashSet<Coordinate>();
             Coordinate currCoordinate = new Coordinate(0, 0);
@@ -84,46 +75,5 @@ namespace Advent
             }
             Console.WriteLine("Distance traveled: " + depth);
         }
-
-        //switch to stack: can deal with dead-ends (loops!) better
-
-        /*static int traverse(ref int index)
-        {
-            Stack<char> currPath = new Stack<char>();
-            int distance = 0;
-            for (; index < regexMap.Length; index++)
-            {
-                if (currPath.Count > 0 && (regexMap[index] == 'W' && currPath.Peek() == 'E' || regexMap[index] == 'E' && currPath.Peek() == 'W' || regexMap[index] == 'S' && currPath.Peek() == 'N' || regexMap[index] == 'N' && currPath.Peek() == 'S'))
-                {
-                    currPath.Pop();
-                    distance--;
-                }
-                else
-                    currPath.Push(regexMap[index]);
-                if (regexMap[index] == '$')
-                    return distance;
-                if (regexMap[index] == ')' || regexMap[index] == '|')
-                {
-                    index++;
-                    return distance;
-                }
-                if (regexMap[index] == '(')
-                {
-                    index++;
-                    return distance + traverseJunction(ref index);
-                }
-                distance++;
-            }
-            return 0; //for final branch
-        }
-
-        static int traverseJunction(ref int index)
-        {
-            int branch1 = traverse(ref index);
-            int branch2 = traverse(ref index);
-            if(branch1 > 0 && branch2 > 0)
-                return Math.Max(branch1, branch2) + traverse(ref index);
-            return Math.Max(Math.Max(branch1, branch2), traverse(ref index));
-        }*/
     }
 }
