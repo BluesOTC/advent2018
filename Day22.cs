c03551ded515e2c58c84dde689b3a13a983b121d
Blues <dotdblues@gmail.com>
2018-12-22 22:57:42 -0500

    reduce nodes checked

--- Day22.cs
+++ Day22.cs
@@ -11,29 +11,21 @@ namespace Advent
         public int terrain;
         public int equipment;
         public int costToNode;
-        int estimatedTimeLeft;
         public int f;
 
         public CaveNode(CaveNode prevNode, int x, int y, int targetX, int targetY, int terrain, int equipment) : base(x, y)
         {
-            this.x = x;
-            this.y = y;
             this.terrain = terrain;
-            this.equipment = equipment >= 3 ? equipment % 3 : equipment;
+            this.equipment = equipment;
             costToNode = calculateCostToNode(prevNode);
-            estimatedTimeLeft = this.findManhattanDistance(targetX, targetY);
-            estimatedTimeLeft += this.equipment == 0 ? 0 : 7;
-            f = costToNode + estimatedTimeLeft;
+            f = costToNode + this.findManhattanDistance(targetX, targetY) + (this.equipment == 0 ? 0 : 7);
         }
 
         int calculateCostToNode(CaveNode prevNode)
         {
             if (prevNode == null)
                 return 0;
-            int g = prevNode.costToNode + 1;
-            if (prevNode.equipment != this.equipment)
-                g += 7;
-            return g;
+            return prevNode.costToNode + (this.equipment == prevNode.equipment ? 1 : 8);
         }
 
         public override bool Equals(object second)
@@ -73,8 +65,8 @@ namespace Advent
                 string[] target = reader.ReadLine().Split(' ', ',');
                 targetX = Int32.Parse(target[1]);
                 targetY = Int32.Parse(target[2]);
-                erosionGrid = new int[(targetY + 1) * 2][];
-                terrainGrid = new int[(targetY + 1) * 2][];
+                erosionGrid = new int[(targetY + 100)][];
+                terrainGrid = new int[(targetY + 100)][];
                 for (int row = 0; row < erosionGrid.Length; row++)
                 {
                     erosionGrid[row] = new int[(targetX + 1) * 2];
@@ -106,7 +98,7 @@ namespace Advent
             openNodes.Add(new CaveNode(null, 0, 0, targetX, targetY, 0, 0)); //start with torch
             while (true)
             {
-                CaveNode currNode = openNodes.OrderBy(x => x.f).FirstOrDefault();
+                CaveNode currNode = openNodes.Take(5).OrderBy(x => x.f).FirstOrDefault();
                 if (currNode.x == targetX && currNode.y == targetY)
                 {
                     if (currNode.equipment == 0)
@@ -129,26 +121,34 @@ namespace Advent
                     if (currNode.x > 0)
                     {
                         terrain = terrainGrid[currNode.y][currNode.x - 1];
-                        tryAddNode(new CaveNode(currNode, currNode.x - 1, currNode.y, targetX, targetY, terrain, currNode.terrain));
-                        tryAddNode(new CaveNode(currNode, currNode.x - 1, currNode.y, targetX, targetY, terrain, currNode.terrain + 1));
+                        if (currNode.equipment != (terrain + 2) % 3)
+                            tryAddNode(new CaveNode(currNode, currNode.x - 1, currNode.y, targetX, targetY, terrain, currNode.equipment));
+                        else
+                            tryAddNode(new CaveNode(currNode, currNode.x - 1, currNode.y, targetX, targetY, terrain, (3 + terrain - currNode.terrain + currNode.equipment) % 3));
                     }
                     if (currNode.y > 0)
                     {
                         terrain = terrainGrid[currNode.y - 1][currNode.x];
-                        tryAddNode(new CaveNode(currNode, currNode.x, currNode.y - 1, targetX, targetY, terrain, currNode.terrain));
-                        tryAddNode(new CaveNode(currNode, currNode.x, currNode.y - 1, targetX, targetY, terrain, currNode.terrain + 1));
+                        if (currNode.equipment != (terrain + 2) % 3)
+                            tryAddNode(new CaveNode(currNode, currNode.x, currNode.y - 1, targetX, targetY, terrain, currNode.equipment));
+                        else
+                            tryAddNode(new CaveNode(currNode, currNode.x, currNode.y - 1, targetX, targetY, terrain, (3 + terrain - currNode.terrain + currNode.equipment) % 3));
                     }
                     if (currNode.y < terrainGrid.Length - 1)
                     {
                         terrain = terrainGrid[currNode.y + 1][currNode.x];
-                        tryAddNode(new CaveNode(currNode, currNode.x, currNode.y + 1, targetX, targetY, terrain, currNode.terrain));
-                        tryAddNode(new CaveNode(currNode, currNode.x, currNode.y + 1, targetX, targetY, terrain, currNode.terrain + 1));
+                        if (currNode.equipment != (terrain + 2) % 3)
+                            tryAddNode(new CaveNode(currNode, currNode.x, currNode.y + 1, targetX, targetY, terrain, currNode.equipment));
+                        else
+                            tryAddNode(new CaveNode(currNode, currNode.x, currNode.y + 1, targetX, targetY, terrain, (3 + terrain - currNode.terrain + currNode.equipment) % 3));
                     }
                     if (currNode.x < terrainGrid[currNode.y].Length - 1)
                     {
                         terrain = terrainGrid[currNode.y][currNode.x + 1];
-                        tryAddNode(new CaveNode(currNode, currNode.x + 1, currNode.y, targetX, targetY, terrain, currNode.terrain));
-                        tryAddNode(new CaveNode(currNode, currNode.x + 1, currNode.y, targetX, targetY, terrain, currNode.terrain + 1));
+                        if (currNode.equipment != (terrain + 2) % 3)
+                            tryAddNode(new CaveNode(currNode, currNode.x + 1, currNode.y, targetX, targetY, terrain, currNode.equipment));
+                        else
+                            tryAddNode(new CaveNode(currNode, currNode.x + 1, currNode.y, targetX, targetY, terrain, (3 + terrain - currNode.terrain + currNode.equipment) % 3));
                     }
                 }
             }
@@ -156,11 +156,11 @@ namespace Advent
 
         static void tryAddNode(CaveNode next)
         {
-            if (closedNodes.Contains(next) || next.equipment == (next.terrain + 2) % 3)
+            if (closedNodes.Contains(next) )
                 return;
+            CaveNode candidate;
             if (openNodes.Contains(next))
             {
-                CaveNode candidate;
                 openNodes.TryGetValue(next, out candidate);
                 candidate.costToNode = Math.Min(candidate.costToNode, next.costToNode);
                 candidate.f = Math.Min(candidate.f, next.f);
