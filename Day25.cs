913bb7874c8b79d0b55d0488c20f98dfe99fd331
Blues <dotdblues@gmail.com>
2018-12-25 09:21:53 -0500

    rewrite day 25 using UnionFind
    ~5x faster

--- Day25.cs
+++ Day25.cs
@@ -6,7 +6,7 @@ using System.IO;
 
 namespace Advent
 {
-    class Point : IComparer<Point>, IComparable<Point>
+    class Point
     {
         public int x, y, z, t;
 
@@ -22,119 +22,70 @@ namespace Advent
         {
             return Math.Abs(this.x - other.x) + Math.Abs(this.y - other.y) + Math.Abs(this.z - other.z) + Math.Abs(this.t - other.t);
         }
-
-        int IComparer<Point>.Compare(Point x, Point y)
-        {
-            Point origin = new Point(0, 0, 0, 0);
-            return x.findManhattanDistance(origin) - y.findManhattanDistance(origin);
-        }
-
-        int IComparable<Point>.CompareTo(Point other)
-        {
-            Point origin = new Point(0, 0, 0, 0);
-            return this.findManhattanDistance(origin) - other.findManhattanDistance(origin);
-        }
-    }
-
-    class TreeNode
-    {
-        public Point self;
-        public TreeNode leftChild;
-        public TreeNode rightChild;
-
-        public TreeNode(Point self, TreeNode leftChild, TreeNode rightChild)
-        {
-            this.self = self;
-            this.leftChild = leftChild;
-            this.rightChild = rightChild;
-        }
     }
 
     class Day25
     {
-        static List<int []> allPoints = new List<int[]>();
         static List<Point> pointList = new List<Point>();
-        static Dictionary<Point, List<Point>> pointsInRange = new Dictionary<Point, List<Point>>();
-        static Dictionary<Point, HashSet<Point>> nearbyPoints = new Dictionary<Point, HashSet<Point>>();
-        static Dictionary<Point, HashSet<Point>> visitedPoints = new Dictionary<Point, HashSet<Point>>();
+        static int[] parentID;
+        static int[] rank;
 
         public static void Run()
         {
             Console.WriteLine("\nDay 25");
-
-            List<HashSet<Point>> constellations = new List<HashSet<Point>>();
+            
             using (StreamReader reader = new StreamReader("input/input25.txt"))
             {
                 string line;
                 while ((line = reader.ReadLine()) != null)
-                    allPoints.Add(line.Split(',').Select(x => int.Parse(x)).ToArray());
+                {
+                    int[] point = line.Split(',').Select(x => int.Parse(x)).ToArray();
+                    pointList.Add(new Point(point[0], point[1], point[2], point[3]));
+                }
             }
-            //Point medianX = allPoints.OrderBy(x => x.x).Skip(allPoints.Count / 2).First();
-
-            //allPoints.Sort();
-            foreach(int[] point in allPoints)
-            {
-                pointList.Add(new Point(point[0], point[1], point[2], point[3]));
-            }
+            parentID = Enumerable.Range(0, pointList.Count).ToArray();
+            rank = Enumerable.Repeat(0, pointList.Count).ToArray();
 
             for (int i = 0; i < pointList.Count; i++)
             {
-                pointsInRange.Add(pointList[i], new List<Point>());
                 for (int j = 0; j < pointList.Count; j++)
                 {
                     if (pointList[i].findManhattanDistance(pointList[j]) <= 3)
-                        pointsInRange[pointList[i]].Add(pointList[j]);
-                }
-            }
-
-            for (int i = 0; i < pointsInRange.Count; i++)
-            {
-                visitedPoints.Add(pointList[i], new HashSet<Point>());
-                nearbyPoints.Add(pointList[i], new HashSet<Point>());
-                foreach (Point p in pointsInRange[pointList[i]])
-                {
-                    addNeighborsOfNeighbor(i, p);
-                }
-            }
-
-            //
-
-            for(int index = 0; index < pointList.Count; index++)
-            {
-                if (constellations.Count == 0)
-                    constellations.Add(new HashSet<Point>(nearbyPoints[pointList[index]]));
-                else
-                {
-                    bool fitsExistingConstellation = false;
-                    foreach (HashSet<Point> constellation in constellations)
                     {
-                        foreach(Point point in nearbyPoints[pointList[index]])
-                        {
-                            if(constellation.Contains(point))
-                            {
-                                fitsExistingConstellation = true;
-                                break;
-                            }
-                        }
-                        if (fitsExistingConstellation)
-                            break;
+                        Union(i, j);
                     }
-                    if (!fitsExistingConstellation)
-                        constellations.Add(new HashSet<Point>(nearbyPoints[pointList[index]]));
                 }
             }
-            Console.WriteLine(constellations.Count);
+
+            int constellations = 0;
+            for (int index = 0; index < parentID.Length; index++)
+            {
+                if (parentID[index] == index)
+                    constellations++;
+            }
+
+            Console.WriteLine(constellations);
         }
 
-        static void addNeighborsOfNeighbor(int index, Point neighbor)
+        static int Find(int i)
         {
-            if (!visitedPoints[pointList[index]].Add(neighbor))
+            return parentID[i] == i ? i : Find(parentID[i]);
+        }
+
+        static void Union(int i, int j)
+        {
+            int rootI = Find(i);
+            int rootJ = Find(j);
+            if (rootI == rootJ)
                 return;
-            nearbyPoints[pointList[index]].Add(neighbor);
-            foreach(Point p in pointsInRange[neighbor])
+            if (rank[rootI] < rank[rootJ])
+                parentID[rootI] = rootJ;
+            else if (rank[rootI] > rank[rootJ])
+                parentID[rootJ] = rootI;
+            else
             {
-                nearbyPoints[pointList[index]].Add(p);
-                addNeighborsOfNeighbor(index, p);
+                parentID[rootI] = rootJ;
+                rank[rootJ]++;
             }
         }
     }
